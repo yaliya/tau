2a2eeb445391eed22a07a57179e0896a958aae9e
Yusuf Aliya <yaliyyaa@gmail.com>
2017-08-11 18:27:01 +0200

    Updated framework

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,6 @@ use Tau\Route;
 
 
 //Api route with custom response
-Route::get("/api/test", "TestController@test")
+Route::post("/api/test", "TestController@test")
       //After middleware
       ->after("TestMiddleware");
